ee89078e847c3399c4703b136a40df86197b3c25
phillipdunn <phillipdunn@hotmail.com>
2022-02-07 16:10:23 +0000

    add with annotation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,62 @@
+// main function to create a card
+const createCard = (cardData) => {
+  //use createElement function to create new html elements
+  const name = createElement('h2', 'card--title');
+  const img = createElement('img', 'card--img', { width: '256' });
+  const card = createElement('li', 'card');
+  const textWrapper = createElement('ul', 'card--text');
+  // use createCardText function to ceate text from the card data
+  const textElements = createCardText(cardData);
+  //add data to some elements
+  name.innerText = cardData.name;
+  img.src = cardData.sprites.other['official-artwork'].front_default;
+  // iterate through card data and append elements to ul tag 'text wrapper'
+  for (const element of textElements) {
+    textWrapper.append(element);
+  }
+  // append the other attributes to the new card
+  card.append(name, img, textWrapper);
+  //return the  new card
+  return card;
+};
 
-console.log(data);
+// dumb function to create elements, pass it the tag type and a classname
+const createElement = (elementType, className = '') => {
+  const newElement = document.createElement(elementType);
+  newElement.className = className;
+  return newElement;
+};
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+// dumb function to add text to element
+const createCardText = (cardInfo) => {
+  const elementsArray = [];
+  //iterate through the card data
+  for (const cardAttr of cardInfo.stats) {
+    //create an li element
+    const element = createElement('li');
+    // add text to the element from card data
+    element.innerText = `${cardAttr.stat.name.toUpperCase()}: ${
+      cardAttr.base_stat
+    }`;
+    // add the new element to the array
+    elementsArray.push(element);
+  }
+  return elementsArray;
+};
+
+// target the '.cards' div in the index html
+const cardsWrapper = document.querySelector('.cards');
+
+// append cards to the '.cards' div
+const appendCard = (cards) => {
+  // iterate through the data
+  for (let i = 0; i < cards.length; i++) {
+    // use create card function for each pokemon in the array
+    const card = createCard(cards[i]);
+    // append this new card to the '.cards div'
+    cardsWrapper.append(card);
+  }
+};
+
+// to initiate the process of creating a card
+appendCard(data);
