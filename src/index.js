c41b69736ab1d9a95d57673a07935a2770f68c1c
Lewis <ljoncb@gmail.com>
2022-02-07 14:49:48 +0000

    core criteria

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
+const createPokemonHTML = (pokemon) => {
+    return `
+    <li class="card">
+        <h2 class="card--title">${pokemon.name}</h2>
+        <img
+            width="256"
+            class="card--img"
+            src="${pokemon.sprites.other['official-artwork'].front_default}"
+        />
+        <ul class="card--text">
+            <li>HP: ${pokemon.stats[0].base_stat}</li>
+            <li>ATTACK: ${pokemon.stats[1].base_stat}</li>
+            <li>DEFENSE: ${pokemon.stats[2].base_stat}</li>
+            <li>SPECIAL-ATTACK: ${pokemon.stats[3].base_stat}</li>
+            <li>SPECIAL-DEFENSE: ${pokemon.stats[4].base_stat}</li>
+            <li>SPEED: ${pokemon.stats[5].base_stat}</li>
+        </ul>
+    `
+}
 
-console.log(data);
+const createPokemonHTMLList = (pokemonList) => {
+    return pokemonList.map(pokemon => createPokemonHTML(pokemon))
+}
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+document.getElementsByClassName('cards')[0].innerHTML = createPokemonHTMLList(data)
+
