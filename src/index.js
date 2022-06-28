8a04d4476224a4239d5b66b46d8bdc82c65e9cd5
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-06-28 13:24:41 +0100

    Core criteria

--- src/index.js
+++ src/index.js
@@ -1,6 +1,62 @@
+// console.log(data);
 
-console.log(data);
+const CARDS_SEL = ".cards";
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+const CARD = "card";
+const CARD__TITLE = "card--title";
+const CARD__IMAGE = "card--img";
+const CARD__TEXT = "card--text";
+
+const IMAGE_WIDTH = 256;
+
+const cardsContainer = document.querySelector(CARDS_SEL);
+
+// NOTE: No longer need this as we use the 'capitalize' text transform
+function uppercaseFirst(s) {
+  if (s.length > 0) {
+    return s[0].toUpperCase() + s.slice(1);
+  } else {
+    return s;
+  }
+}
+
+// That's what they are :)
+const monsters = data;
+
+for (let monster of monsters) {
+  const cardEl = document.createElement("li");
+  cardEl.className = CARD;
+  // Turn off list bullets
+  cardEl.style.listStyle = "none";
+
+  const nameEl = document.createElement("h2");
+  nameEl.className = CARD__TITLE;
+  nameEl.style.textTransform = "capitalize";
+  nameEl.innerText = monster.name;
+  cardEl.append(nameEl);
+
+  const imgEl = document.createElement("img");
+  imgEl.className = CARD__IMAGE;
+  imgEl.width = IMAGE_WIDTH;
+  imgEl.src = monster.sprites.other["official-artwork"].front_default;
+  cardEl.append(imgEl);
+
+  const ulEl = document.createElement("ul");
+  ulEl.className = CARD__TEXT;
+  // Turn off list bullets
+  ulEl.style.listStyle = "none";
+  // Some styling
+  ulEl.style.lineHeight = 1.5;
+  ulEl.style.paddingLeft = "5rem";
+  cardEl.append(ulEl);
+
+  for (let stat of monster.stats) {
+    const liEl = document.createElement("li");
+    liEl.innerText = stat.stat.name.toUpperCase() + ": " + stat.base_stat;
+
+    ulEl.append(liEl);
+  }
+
+  // Add a completed monster card
+  cardsContainer.append(cardEl);
+}
